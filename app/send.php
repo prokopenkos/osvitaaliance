61043a1f5b3e4438d5a64e6c8e84d11c37949e13
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-06-22 19:27:08 +0300

    Added google map api

--- app/send.php
+++ app/send.php
@@ -3,4 +3,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     if (filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)){
         mail('giperion.od@gmail.com','Вопрос от сайта'.$_POST['email'],''.$_POST['email'].$_POST['name'].$_POST['subject'].$_POST['message']);
     }
+}else{
+    echo "TEst mode";
+    if(mail('web.prokopenko@gmail.com','subject','message','')) echo "TEST OK";
 }
